4118dc04e80856a37874d52c6b89efd1d797aa1d
Mike Perryman <32601976+perrymanmd@users.noreply.github.com>
2025-08-05 14:06:19 -0500

    WIP on RTD Setup

--- rtd_docs/conf.py
+++ rtd_docs/conf.py
@@ -19,7 +19,7 @@ with pyproject_path.open("rb") as f:
 
 release = pyproject["project"]["version"]
 project = f'hec-python-library {release}'
-copyright = '2025, CEIWR-HEC'
+copyright = ''
 author = 'CEIWR-HEC-WM'
 
 # -- General configuration ---------------------------------------------------
