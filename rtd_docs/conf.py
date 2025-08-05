732c53a54ca33f336de6ea98236a5f70a90353ca
Mike Perryman <32601976+perrymanmd@users.noreply.github.com>
2025-08-05 14:32:14 -0500

    Update conf.py

--- rtd_docs/conf.py
+++ rtd_docs/conf.py
@@ -19,7 +19,7 @@ with pyproject_path.open("rb") as f:
 
 release = pyproject["project"]["version"]
 project = f'hec-python-library {release}'
-copyright = 'Not Copyrighted'
+copyright = 'None'
 author = 'CEIWR-HEC-WM'
 
 # -- General configuration ---------------------------------------------------
