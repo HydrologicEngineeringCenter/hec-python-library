6e9d25e044a84e94c337323aa61e740276116c4d
Mike Perryman <32601976+perrymanmd@users.noreply.github.com>
2025-08-05 14:34:16 -0500

    Update conf.py

--- rtd_docs/conf.py
+++ rtd_docs/conf.py
@@ -38,11 +38,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-
 html_static_path = ["_static"]
-html_css_files = [
-    "custom.css",
-]
 
 # -- Generate a base parameter table -----------------------------------------
 
